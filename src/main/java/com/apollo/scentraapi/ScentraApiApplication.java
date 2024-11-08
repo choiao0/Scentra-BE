ec76ef62cfdf95178df6c45435af6cea74e56933
iyeongseo <iyeongssseo@gmail.com>
2024-11-08 19:44:38 +0900

    initialize project

---
+++ src/main/java/com/apollo/scentraapi/ScentraApiApplication.java
@@ -0,0 +1,13 @@
+package com.apollo.scentraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScentraApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScentraApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apollo/scentraapi/ScentraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollo.scentraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScentraApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
