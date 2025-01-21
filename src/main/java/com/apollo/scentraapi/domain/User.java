23baca00d049f2926f89f243b0dbc24647303496
iyeongseo <iyeongssseo@gmail.com>
2025-01-21 11:17:30 +0900

    refactor: 오타 수정

--- src/main/java/com/apollo/scentraapi/domain/User.java
+++ src/main/java/com/apollo/scentraapi/domain/User.java
@@ -24,7 +24,7 @@ public class User {
     private String gender;
 
     @Column(name="created_at")
-    private LocalDateTime created_at;
+    private LocalDateTime createdAt;
 
     @Column(name="updated_at")
     private LocalDateTime updatedAt;
