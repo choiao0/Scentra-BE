3f1bf40c7b0da89bbbed24bf30d71056ac83ab4e
choiao0 <qawse6@daum.net>
2025-11-16 04:03:54 +0900

    chore(UserRepositoryTest): BeforeEach 어노테이션 BeforeAll로 변경

--- src/test/java/com/apollo/scentraapi/repository/UserRepositoryTest.java
+++ src/test/java/com/apollo/scentraapi/repository/UserRepositoryTest.java
@@ -5,9 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.apollo.scentraapi.domain.User;
 import com.apollo.scentraapi.domain.enums.Gender;
 import java.util.Optional;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,17 +17,15 @@ import org.springframework.test.context.ActiveProfiles;
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
-    @BeforeEach
+    @BeforeAll
     void setUp() {
-        userRepository.deleteAll();
-
         User user = createUser("user", "user@example.com", Gender.MALE);
-
         userRepository.save(user);
     }
 
