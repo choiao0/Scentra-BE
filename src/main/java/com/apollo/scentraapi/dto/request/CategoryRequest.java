8d2a9abe4c30dc9dd19e115c348d480bdf9ad0ac
choiao0 <qawse6@daum.net>
2025-11-23 16:44:44 +0900

    test(CategoryServiceTest): 테스트 코드 작성

--- src/main/java/com/apollo/scentraapi/dto/request/CategoryRequest.java
+++ src/main/java/com/apollo/scentraapi/dto/request/CategoryRequest.java
@@ -1,15 +1,13 @@
 package com.apollo.scentraapi.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 
 public class CategoryRequest {
 
-    @Builder
     @Getter
     public static class CategoryNameDto {
-        private String categoryNameKr;
-        private String categoryNameEn;
-        private String categoryType;
+        String categoryNameKr;
+        String categoryNameEn;
+        String categoryType;
     }
 }
